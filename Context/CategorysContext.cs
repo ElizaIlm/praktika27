36624bf2bd668f6bc17937be367940e9f0a520d9
student-v424 <student-v424@STUD-RM6-V424>
2026-03-16 10:59:36 +0300

    Разработанные контексты данных.

---
+++ Context/CategorysContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika27.Context
+{
+    public class CategorysContext : Categorys
+    {
+        public static ObservableCollection<CategorysContext> AllCategorys()
+        {
+            ObservableCollection<CategorysContext> allCategories = new ObservableCollection<CategorysContext>();
+            SqlConnection connection;
+            SqlDataReader dataCategorys = Connection.Query("SELECT * FROM [dbo].[Categorys]", out connection);
+            while (dataCategorys.Read())
+            {
+                allCategorys.Add(new CategorysContext()
+                {
+                    Id = dataCategorys.GetInt32(0), 
+                    Name = dataCategorys.GetString(1) 
+                });
+            }
+            Connection.CloseConnection(connection);
+            return allCategorys;
+        }
+    }
+}
