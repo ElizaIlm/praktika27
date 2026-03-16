3c2d1d8afbd1d348ebd74ac497663b47e79f22b5
gobdanov <gobdanov@inbox.ru>
2026-03-16 11:46:04 +0500

    Разработанные классы общего пользования.

---
+++ Classes/Connection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika27.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=;" +
+            "Trusted_Connection=No;" +
+            "DataBase=ShopContent;" +
+            "User= ;" +
+            "PWD= ;";
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public static SqlDataReader Query(string SQL, out SqlConnection connection )
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
